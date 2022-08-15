f8ef7c337c9d19db0f87b6bc15b58c6c8ec69661
hasindi <hmudithya@gmail.com>
2022-08-15 23:34:18 +0530

    Created connection and use bufferReader and printWriter

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -7,16 +7,31 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ClientFormController extends Thread{
     public TextField txtTypemsg;
     public VBox vBox;
     public Label lblUserName;
     public ImageView camera;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+    private Socket socket;
 
     public void initialize(){
         String userName = LoginFormController.userName;
         lblUserName.setText(userName);
 
+        try {
+            socket = new Socket("localhost", 8000);
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            printWriter = new PrintWriter(socket.getOutputStream(), true);
+            this.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendOnAction(ActionEvent actionEvent) {
