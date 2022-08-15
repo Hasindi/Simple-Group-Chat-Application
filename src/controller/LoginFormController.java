ce7f9734527fbb691598839ee8c71da3f8023ecb
hasindi <hmudithya@gmail.com>
2022-08-15 23:19:05 +0530

    Initialize loginform Controller

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -7,7 +7,10 @@ import javafx.scene.control.TextField;
 public class LoginFormController {
     public TextField txtLogin;
     public Button btnLogin;
+    public static String userName;
 
     public void loginOnAction(ActionEvent actionEvent) {
+        userName = txtLogin.getText();
+
     }
 }
