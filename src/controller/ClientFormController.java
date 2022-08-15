ae4277ca485d3a1bfe670c587c3abf83068c6654
hasindi <hmudithya@gmail.com>
2022-08-15 21:58:21 +0530

    Created User Interface

---
+++ src/controller/ClientFormController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class ClientFormController {
+    public TextField txtTypemsg;
+    public VBox vBox;
+    public Label lblUserName;
+    public ImageView camera;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SendImageClickOnAction(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtLogin;
+    public Button btnLogin;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+}
