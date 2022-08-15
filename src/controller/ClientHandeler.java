8c769550559bc00728e3e9b3f9521f84f9433fb7
hasindi <hmudithya@gmail.com>
2022-08-15 22:55:17 +0530

    Import variable(BufferReader,BufferRider,ArrayList) and create method

--- src/controller/ClientHandeler.java
+++ src/controller/ClientHandeler.java
@@ -1,4 +1,19 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ClientHandeler extends Thread{
+    public static ArrayList<ClientHandeler> clientHandlers;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+    private PrintWriter printWriter;
+
+    public ClientHandeler(Socket accept, ArrayList<ClientHandeler> clientHandelers) {
+    }
 }
