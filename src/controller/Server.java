7809f844005c3e639fb78de66a6d5d2054073887
hasindi <hmudithya@gmail.com>
2022-08-15 22:44:45 +0530

    Created variable for connected to client

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,8 +1,20 @@
 package controller;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
     private static ArrayList<ClientHandeler> clientHandelers = new ArrayList<>();
 
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8000);
+        Socket accept;
+
+        while (true) {
+            System.out.println("Waiting for Client.....");
+            accept = serverSocket.accept();
+        }
+    }
 }
