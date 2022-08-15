3c8c290402c72cfa6affd6fa4318bb6b0b634bef
hasindi <hmudithya@gmail.com>
2022-08-15 23:22:38 +0530

    Finish loginForm Controller

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,16 +1,29 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField txtLogin;
     public Button btnLogin;
     public static String userName;
 
-    public void loginOnAction(ActionEvent actionEvent) {
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
         userName = txtLogin.getText();
+        Stage stage = (Stage) txtLogin.getScene().getWindow();
+        stage.close();
 
+        Stage stage1 = new Stage();
+        stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/views/ClientForm.fxml"))));
+        stage1.setTitle("Let's Chat....");
+        stage1.setResizable(false);
+        stage1.centerOnScreen();
+        stage1.show();
     }
 }
